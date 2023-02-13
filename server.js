4089e141a6caec5f4e2e4d8d7b006606872199f1
flatherz <122326493+flatherz@users.noreply.github.com>
2023-02-13 15:36:47 -0800

    Update server.js

--- server.js
+++ server.js
@@ -25,13 +25,9 @@ const twitchSecretToken = process.env.CLIENT_SECRET;
 let client; // forward declaration of variable
 
 // CITE: https://github.com/jgdigitaljedi/video-game-db-supplemental-data/blob/master/server/igdb.controller.js
-=======
-const twitchClientId = process.env.CLIENT_ID; // 'qjtdnsfn9pqsdybu2zhwrhsq46vl2q';
-const twitchSecretToken = process.env.CLIENT_SECRET; // 'w23yddb384nkoinridzgysgg7axqv0'; 
+const twitchClientId = process.env.CLIENT_ID; 
+const twitchSecretToken = process.env.CLIENT_SECRET;
 let client; // forward declaration of variable
-
-// from: https://github.com/jgdigitaljedi/video-game-db-supplemental-data/blob/master/server/igdb.controller.js
->>>>>>> 9b1a205e5a94c09253e8c6c7095e6e9e7b140f6f
 
 // helper functions
 async function getAppAccessToken() {
@@ -40,33 +36,19 @@ async function getAppAccessToken() {
   );
 }
 
-<<<<<<< HEAD
 // Refresh API key
-=======
-//
->>>>>>> 9b1a205e5a94c09253e8c6c7095e6e9e7b140f6f
 async function refreshAppKey() {
     try {
         const appKeyRes = await getAppAccessToken();
         appKey = appKeyRes.data;
-<<<<<<< HEAD
-=======
-        //console.log('clientId', twitchClientId);
-        //console.log('token', appKey.access_token);
->>>>>>> 9b1a205e5a94c09253e8c6c7095e6e9e7b140f6f
         return igdb(twitchClientId, appKey.access_token);
     } catch (e) {
         console.error(e);
     }
 }
 
-<<<<<<< HEAD
 // Helper search function 
-async function fuzzyApiSearch(game) {
-=======
-// 
 async function apiSearch(game) {
->>>>>>> 9b1a205e5a94c09253e8c6c7095e6e9e7b140f6f
     if (!client || !appKey) {
       client = await refreshAppKey();
     }
@@ -76,18 +58,6 @@ async function apiSearch(game) {
       .request('/games');
   }
 
-<<<<<<< HEAD
-=======
- /* async function apiCoverArt(coverId) {
-    if (!client || !appKey) {
-      client = await refreshAppKey();
-    }
-    return client
-      .fields(`url`)
-      .request('/covers');
-  } */
-
->>>>>>> 9b1a205e5a94c09253e8c6c7095e6e9e7b140f6f
 // route handlers
 app.get('/', (req, res) => {
     refreshAppKey();
@@ -96,16 +66,6 @@ app.get('/', (req, res) => {
 
 app.get('/search', async (req, res) => {
     const name = req.query.game;
-<<<<<<< HEAD
-    console.log(name);
-    try {
-        if (name) {
-        const apiResults = await fuzzyApiSearch(name);
-        const data = apiResults.data;
-        res.json(data);
-        } else {
-        res.status(500).json({ error: true, message: 'What is your game called?' });
-=======
     try {
         if (name) {
           const searchResults = await apiSearch(name);
@@ -113,7 +73,6 @@ app.get('/search', async (req, res) => {
           res.send(data);
         } else {
           res.status(500).json({ error: true, message: 'What is your game called?' });
->>>>>>> 9b1a205e5a94c09253e8c6c7095e6e9e7b140f6f
         }
     } catch (e) {
         console.error(e);
